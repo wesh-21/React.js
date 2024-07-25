62fda56641047d28075cbdd4d728383109c5f200
Miguel Bibi <miguel.bibi@tecnico.ulisboa.pt>
2024-07-25 06:11:42 +0100

    fix: repo display

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+function App() {
+  const [todos, setTodos] = useState([
+    'Go to the MMA class',
+    'Learn React',
+    'Do the laundry'
+  ])
+  const [todoValue, setTodoValue] = useState('')
+
+
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(value, index) {
+    setTodoValue(value)
+    handleDeleteTodo(index)
+  }
+
+
+  return (
+    <>
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+      <TodoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import TodoCard from './TodoCard'
+
+export default function TodoList(props) {
+    const { todos } = props
+
+
+    return (
+        <ul className='main'>
+            {todos.map((todo, todoIndex) => {
+                return (
+                    <TodoCard todo={todo} {...props} key={todoIndex} index={todoIndex}>
+                        <p>{todo}</p>
+                    </TodoCard>
+                )
+            })}
+        </ul>
+    )
+}
